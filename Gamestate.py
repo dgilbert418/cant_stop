63daca8d56f2c5d27c4f09af40e16a50bda5cd36
Daniel Gilbert <dgilbert@gmail.com>
2020-03-19 04:55:34 -0700

    Style changes to Gamestate

--- Gamestate.py
+++ Gamestate.py
@@ -20,12 +20,12 @@ class Gamestate:
         player_progress = []
         for i in range(num_players):
             p = {}
-            for j in __class__.LANE_LENGTHS:
+            for j in self.LANE_LENGTHS:
                 p[j] = 0
             player_progress.append(p)
         turn_progress = {}
         cur_player = start_player
-        dice = [0 for i in range(__class__.NUM_DICE)]
+        dice = [0 for i in range(self.NUM_DICE)]
         board = class_object(player_progress, turn_progress, cur_player, dice)
         board.roll_dice()
         return board
@@ -40,14 +40,14 @@ class Gamestate:
                 "Cannot advance on lane " + str(lane) + "; it is already complete."
             )
         elif lane in self.turn_progress:
-            if self.player_progress[self.cur_player][lane] + self.turn_progress[lane] < __class__.LANE_LENGTHS[lane]:
+            if self.player_progress[self.cur_player][lane] + self.turn_progress[lane] < self.LANE_LENGTHS[lane]:
                 self.turn_progress[lane] += 1
             else:
                 raise CannotAdvanceError(
                     "Cannot advance on lane " + str(lane) + "; end of lane."
                 )
         else:
-            if len(self.turn_progress) >= __class__.NUM_TEMP_CONES:
+            if len(self.turn_progress) >= self.NUM_TEMP_CONES:
                 raise CannotAdvanceError(
                     "Cannot advance on lane " + str(lane) + "; no more temporary cones."
                 )
@@ -66,7 +66,7 @@ class Gamestate:
         game_dice = sorted(self.dice)
         if combo_dice != game_dice:
             raise InvalidCombinationError("Dice in combination do not match current game dice.")
-        if not all([__class__.is_die_face(i) for i in combo_dice]):
+        if not all([self.is_die_face(i) for i in combo_dice]):
             raise InvalidCombinationError("Combination contains a die which is not a valid die face.")
         lanes = [sum(i) for i in combo]
         if not any([self.is_lane_advanceable(i) for i in lanes]):
@@ -106,30 +106,22 @@ class Gamestate:
 
     def find_completed_lanes(self):
         complete = []
-        for i in __class__.LANE_LENGTHS:
+        for i in self.LANE_LENGTHS:
             for j in range(len(self.player_progress)):
                 if (j == self.cur_player) and (i in self.turn_progress):
-                    if self.player_progress[j][i] + self.turn_progress[i] == __class__.LANE_LENGTHS[i]:
+                    if self.player_progress[j][i] + self.turn_progress[i] == self.LANE_LENGTHS[i]:
                         complete.append(i)
                 else:
-                    if self.player_progress[j][i] == __class__.LANE_LENGTHS[i]:
+                    if self.player_progress[j][i] == self.LANE_LENGTHS[i]:
                         complete.append(i)
         return complete
+
 
 class CannotAdvanceError(Exception):
     def __init__(self, message):
         print(message)
 
+
 class InvalidCombinationError(Exception):
     def __init__(self, message):
         print(message)
-
-
-
-
-
-
-
-
-
-
