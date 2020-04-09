7e8c1ba066d60727f50c4b4b0e86718d99ad2dd5
Kyle Krowpman <kyle.stuart.krowpman@cern.ch>
2020-04-08 23:57:22 -0400

    Jeremy_Bot

--- teststuff.py
+++ teststuff.py
@@ -6,23 +6,10 @@ from GameManager import GameManager
 bad_players = [Jeremy_Bot(), Player()]
 my_game = GameManager(bad_players)
 winners = {0: 0, 1: 0}
-for i in range(10000):
+for i in range(1000):
+    if i % 1000 == 0:
+        print(i)
     winner = my_game.execute_game()
     winners[winner] += 1
 print(winners)
 
-
-# gamestate = Gamestate.new_board(4, 0)
-# gamestate.print_state()
-# gamestate.advance([2, 7])
-# gamestate.print_state()
-# gamestate.advance([2, 8])
-# gamestate.print_state()
-# gamestate.lock_in()
-# gamestate.next_player()
-# gamestate.print_state()
-# gamestate.advance([4,5])
-# gamestate.advance([4,5])
-# gamestate.lock_in()
-# gamestate.next_player()
-# gamestate.print_state()
